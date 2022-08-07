e9f5af6f58fc0674519a224540fa17aa573e7d5a
dicksonagyei <64383297+dicksonagyei@users.noreply.github.com>
2022-08-07 21:59:26 +0000

    Nearest factorial to a number
    Find the nearest factorial to any number with offset (difference)

---
+++ nearest_factorial.py
@@ -0,0 +1,27 @@
+# Function to get the nearest factorial to a number including its offset - By Dickson Agyei
+def nearest_factorial(num):
+    steps = []
+    n = 1
+    fact = 1
+    stop = False
+    while not stop:
+        old_fact = fact
+        fact *= n
+
+        if fact >= num:
+            offset1 = fact - num
+            offset2 = num - old_fact
+
+            if offset1 < offset2:
+                steps.append((n, -offset1))
+            else:
+                steps.append((n - 1, offset2))
+
+            stop = True
+        else:
+            n += 1
+
+    return steps
+
+
+print(nearest_factorial(int(input("Enter Number: "))))
